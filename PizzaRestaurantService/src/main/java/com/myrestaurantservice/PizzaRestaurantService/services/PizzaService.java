bcc8a032c7f08cc034d623985d87542bcd68ac28
Enrico Matrullo <80920594+HenryMatru@users.noreply.github.com>
2023-01-23 14:41:50 +0100

    Delete PizzaRestaurantService directory

--- PizzaRestaurantService/src/main/java/com/myrestaurantservice/PizzaRestaurantService/services/PizzaService.java
+++
@@ -1,7 +0,0 @@
-package com.myrestaurantservice.PizzaRestaurantService.services;
-
-import com.myrestaurantservice.PizzaRestaurantService.models.Pizza;
-
-public interface PizzaService extends GenericService<Pizza, Long> {
-
-}
