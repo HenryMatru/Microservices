8f87a0f76569ae2ae49275f453ecc37f82d1a41f
Enrico Matrullo <80920594+HenryMatru@users.noreply.github.com>
2023-01-25 10:16:27 +0100

    Delete Esercizio3/PizzaService directory

--- Esercizio3/PizzaService/src/main/java/com/myrestaurant/store/PizzaService/services/ToppingService.java
+++
@@ -1,7 +0,0 @@
-package com.myrestaurant.store.PizzaService.services;
-
-import com.myrestaurant.store.PizzaService.models.Topping;
-
-public interface ToppingService extends GenericService<Topping, Long> {
-
-}
