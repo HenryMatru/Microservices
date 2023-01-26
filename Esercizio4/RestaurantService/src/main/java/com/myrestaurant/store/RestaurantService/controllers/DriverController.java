6e54e05de8a23a6f5712eece69a0d15b1d958896
Enrico Matrullo <80920594+HenryMatru@users.noreply.github.com>
2023-01-26 15:02:28 +0100

    Add files via upload

---
+++ Esercizio4/RestaurantService/src/main/java/com/myrestaurant/store/RestaurantService/controllers/DriverController.java
@@ -0,0 +1,30 @@
+package com.myrestaurant.store.RestaurantService.controllers;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.myrestaurant.store.RestaurantService.dto.DriverDTO;
+
+import java.util.List;
+
+@Api(tags = "Driver API")
+public interface DriverController {
+
+    @ApiOperation("Add new Driver")
+    public DriverDTO save(@RequestBody DriverDTO driverDTO);
+
+    @ApiOperation("Find Driver by ID")
+    public DriverDTO findById(@PathVariable("id") Long id);
+
+    @ApiOperation("Delete Driver by ID")
+    public void delete(@PathVariable("id") Long id);
+
+    @ApiOperation("Find all Drivers")
+    public List<DriverDTO> list();
+
+    @ApiOperation("Update Driver")
+    public DriverDTO update(@RequestBody DriverDTO driverDTO, @PathVariable("id") Long id);
+
+}
