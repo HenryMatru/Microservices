2800f89a7f568e1739ee8db89d4ac0027c6d57a9
Enrico Matrullo <80920594+HenryMatru@users.noreply.github.com>
2023-01-26 18:06:57 +0100

    Add files via upload

---
+++ Esercizio5/PizzaService/src/main/java/com/myrestaurant/store/PizzaService/controllers/impl/ToppingControllerImpl.java
@@ -0,0 +1,67 @@
+package com.myrestaurant.store.PizzaService.controllers.impl;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.myrestaurant.store.PizzaService.controllers.ToppingController;
+import com.myrestaurant.store.PizzaService.dto.ToppingDTO;
+import com.myrestaurant.store.PizzaService.mappers.ToppingMapper;
+import com.myrestaurant.store.PizzaService.models.Topping;
+import com.myrestaurant.store.PizzaService.services.ToppingService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/toppings")
+@RequiredArgsConstructor
+public class ToppingControllerImpl implements ToppingController {
+
+    private final ToppingService toppingService;
+
+    private final ToppingMapper toppingMapper;
+
+    @Override
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ToppingDTO save(@RequestBody ToppingDTO toppingDTO) {
+        Topping topping = toppingMapper.asEntity(toppingDTO);
+        return toppingMapper.asDTO(toppingService.save(topping));
+    }
+
+    @Override
+    @GetMapping("/{id}")
+    public ToppingDTO findById(@PathVariable("id") Long id) {
+        Topping topping = toppingService.findById(id).orElse(null);
+        return toppingMapper.asDTO(topping);
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        toppingService.deleteById(id);
+    }
+
+    @Override
+    @GetMapping
+    public List<ToppingDTO> list() {
+        return toppingMapper.asDTOList(toppingService.findAll());
+    }
+
+    @Override
+    @PutMapping("/{id}")
+    public ToppingDTO update(@RequestBody ToppingDTO toppingDTO, @PathVariable("id") Long id) {
+        Topping topping = toppingMapper.asEntity(toppingDTO);
+        return toppingMapper.asDTO(toppingService.update(topping, id));
+    }
+
+}
---
+++ Esercizio5/PizzaService/src/main/java/com/myrestaurant/store/PizzaService/models/Pizza.java
@@ -0,0 +1,65 @@
+package com.myrestaurant.store.PizzaService.models;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+@Builder
+@Table(name = "pizzas")
+public class Pizza implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pizza_id")
+    private Long id;
+
+    @NotBlank
+    @Size(max = 255)
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "favorite")
+    private boolean favorite;
+    
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+            name = "pizzas_toppings",
+            joinColumns = @JoinColumn(
+                    name = "pizza_id", referencedColumnName = "pizza_id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "topping_id", referencedColumnName = "topping_id"
+            )
+    )
+    @JsonIgnore
+    private Set<Topping> toppings = new HashSet<>();
+
+}
